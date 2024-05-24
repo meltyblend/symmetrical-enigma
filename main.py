706f4803ef1ab3d9ce17f9e88011bcd3a4a055e7
meltyblend <aren.jpg@gmail.com>
2024-05-24 11:10:11 -0700

    Simple neural network from grokking Chapter 3

---
+++ main.py
@@ -0,0 +1,18 @@
+#grokking Deep Learning Chapter 3
+#Project 1
+
+#aren chavez
+
+import numpy as np
+
+weight = 0.1
+
+def neural_network(input, weight):
+    prediction = input * weight
+    return prediction
+
+number_of_toes = [8.5, 9.5, 10, 9]
+input = number_of_toes[0]
+
+pred = neural_network(input, weight)
+print(pred)
